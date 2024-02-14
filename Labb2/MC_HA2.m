ac26554f50777ac875240691577336d1b9ccbf95
Carl Erneman <carlerneman@MacBook-Air-som-tillhor-Carl.local>
2024-02-14 14:16:54 +0100

    Task3

--- Labb2/MC_HA2.m
+++ Labb2/MC_HA2.m
@@ -6,9 +6,9 @@ clc; clear;
 
 disp("3). Sequential Importance Sampling, Naive approach")
 
-n = 5 + 1; %Nbr of steps +1(to ignore the initial state)
+n = 10 + 1; %Nbr of steps +1(to ignore the initial state)
 d = 2; %Dimensions
-N = 100000; %Nbr of particles
+N = 10000; %Nbr of particles
 
 
 X = zeros(n, d, N);
@@ -26,6 +26,8 @@ for particle = 1:N
         %error counter
         if ~ismember(X(stepnbr, :, particle), X(1:stepnbr-1, :, particle), 'rows')
             N_sa(stepnbr, 1) = N_sa(stepnbr, 1) + 1;
+        else
+            break;
         end
     end
 end
@@ -39,4 +41,39 @@ disp(N_sa(2:n, 1))
 
 fprintf('c_n from 1 to %i steps: \r\n', n-1)
 disp(c_n(2:n, 1))
+
+
+
+
+%% 4). 
+
+disp("-----------------------------------------------------------------")
+disp("Improved Sequential Importance Sampling, g=SAW")
+
+n = 10 + 1; %Nbr of steps +1(to ignore the initial state)
+d = 2; %Dimensions
+N = 10000; %Nbr of particles
+
+
+X = zeros(n, d, N);
+dir_mat = [eye(d); -1*eye(d)];
+N_sa = zeros(n,1);
+c_n = zeros(n,1);
+
+
+for particle = 1:N
+    for stepnbr = 2:n
+        dir = datasample(dir_mat,1);
+        X(stepnbr, :, particle) = X(stepnbr-1, :, particle) + dir;
+
+        %Check new step against all old steps. If not SAW, increase
+        %error counter
+        if ~ismember(X(stepnbr, :, particle), X(1:stepnbr-1, :, particle), 'rows')
+            N_sa(stepnbr, 1) = N_sa(stepnbr, 1) + 1;
+        else
+            break;
+        end
+    end
+end
+
 
